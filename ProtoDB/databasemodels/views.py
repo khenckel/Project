cdb125ac71cedfcc22023740c5cf3e681b7494a4
khenckel <khenckel@hotmail.com>
2014-08-27 15:23:22 -0400

    Update views.py

--- ProtoDB/databasemodels/views.py
+++ ProtoDB/databasemodels/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the poll database.")
+    
+def details(request, protocol_id):
+    return HttpResponse("You're looking at the details for protocol %s." % protocol_id)
+
+def procedure(request, protocol_id):
+    return HttpResponse("You're looking at the procedure for protocol %s." % protocol_id)
+
+def rating(request, protocol_id):
+    return HttpResponse("You're rating protocol %s." % protocol_id)
+    
+def favorite(request, protocol_id):
+    return HttpResponse("You're favoriting protocol %s." % protocol_id)
